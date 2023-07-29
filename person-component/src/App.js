ca0cdc4573bc01676da1b3e22680e5db87ac4458
Amannroy <royaman56456@gamil.com>
2023-07-29 11:16:59 +0530

    Committing person component

---
+++ person-component/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Person from './Person';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>People Component</h1>
+      <Person name="John" age="30" />
+      
+    </div>
+  );
+}
+
+export default App;
